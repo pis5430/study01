1672519e21063bffbe87f7766954e6e60841e045
pyo i seul <isl1089@naver.com>
2020-11-27 18:22:50 +0900

    추가

--- src/com/javastudy/chapter06_1/SutdaCard.java
+++ src/com/javastudy/chapter06_1/SutdaCard.java
@@ -1,35 +1,25 @@
 package com.javastudy.chapter06_1;
 
-public class SutdaCard {
+public class SutdaCard { //다시풀어보기
 	
 	private int num;
-
 	private boolean isKwang;
 
 
-
 	public SutdaCard(){
-
 		this(1, true); //SutdaCard(1,true)를 호출한다.
-
 	}
 
 
-
 	public SutdaCard(int num, boolean isKwang){
-
 		this.num = num;
-
 		this.isKwang = isKwang;
-
 	}
 
 
 
 	public String info(){
-
-		return num + (isKwang ? "K" : "");
-
+		return num + (isKwang ?"K": "");
 	}
 	
 	
